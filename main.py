61752a045d8f124ef7a319fea7ade9e740aedb1a
quantx13 <93047140+shlayb@users.noreply.github.com>
2025-04-18 03:09:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ async def show_porto(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def cek(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    symbols = ["ADRO", "ENRG"]
+    symbols = ["ADRO", "ENRG", "ACES"]
     message = ""
 
     for symbol in symbols:
